17d3150c6e40402446e9002ef1f6cddec825a332
Igor Caetano Diniz <100551021+igorconsulting@users.noreply.github.com>
2025-04-30 19:42:15 -0300

    Update first_calculator.py
    commit considering natural reading and private methods

--- service/first_calculator.py
+++ service/first_calculator.py
@@ -5,10 +5,19 @@ class FirstCalculator:
     def __init__(self,value: float):
         self.value = value
 
-    def divide(self,value: float)-> list[float]:
+    def calculate(self) -> float:
+        v1,v2,v3 = self.__divide(self.value)
+
+        r1 = self.__part_1(v1)
+        r2 = self.__part_2(v2)
+        r3 = self.__part_3(v3)
+
+        return self.__part_4([r1,r2,r3])
+
+    def __divide(self,value: float)-> list[float]:
         return [value/3,value/3,value/3]
     
-    def part_1(self,value: float) -> float:
+    def __part_1(self,value: float) -> float:
         result = value/4
         result = result+7
         result = math.sqrt(result)
@@ -16,23 +25,15 @@ class FirstCalculator:
 
         return result
     
-    def part_2(self,value: float) -> float:
+    def __part_2(self,value: float) -> float:
         result = math.pow(value,2.121)
         result/=5
         result+=1
         return result
     
-    def part_3(self,value: float) -> float:
+    def __part_3(self,value: float) -> float:
         return value
     
-    def part_4(self,value_list:list[float]) -> float:
+    def __part_4(self,value_list:list[float]) -> float:
         return mean(value_list)
     
-    def calculate(self) -> float:
-        v1,v2,v3 = self.divide(self.value)
-
-        r1 = self.part_1(v1)
-        r2 = self.part_2(v2)
-        r3 = self.part_3(v3)
-
-        return self.part_4([r1,r2,r3])
